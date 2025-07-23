c7d8890521f2cfc08247440d771d01f9b17a15af
herliananur <herliananur19@gmail.com>
2025-07-23 14:22:19 +0800

    ubah ukuran height

--- lib/shared/widgets/appbar_widget.dart
+++ lib/shared/widgets/appbar_widget.dart
@@ -12,17 +12,15 @@ PreferredSizeWidget appbarBasicWidget(BuildContext context,
       titleSpacing: 0,
       automaticallyImplyLeading: false,
       title: Center(
-        child: ConstrainedBox(
-          constraints: const BoxConstraints(maxWidth: 375),
-          child: Container(
-            alignment: Alignment.centerLeft,
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Text(
-              title,
-              style: mainBody3.copyWith(
-                color: Colors.white,
-                fontWeight: FontWeight.w700,
-              ),
+        child: Container(
+          width: double.infinity,
+          alignment: Alignment.centerLeft,
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Text(
+            title,
+            style: mainBody3.copyWith(
+              color: Colors.white,
+              fontWeight: FontWeight.w700,
             ),
           ),
         ),
@@ -31,41 +29,47 @@ PreferredSizeWidget appbarBasicWidget(BuildContext context,
   );
 }
 
-AppBar appbarWidget(BuildContext context,
-    {required String title,
-    Widget? suffixWidget,
-    Color? customColor,
-    Color? customForeground}) {
-  return AppBar(
-    elevation: 1,
-    backgroundColor: customColor ?? Colors.white,
-    titleSpacing: 0,
-    automaticallyImplyLeading: false,
-    title: Container(
-      padding: EdgeInsets.symmetric(horizontal: margin16),
-      child: Row(
-        children: [
-          GestureDetector(
-            onTap: () {
-              Navigator.pop(context);
-            },
-            child: Icon(
-              Icons.arrow_back,
-              color: customForeground ?? Theme.of(context).primaryColor,
+PreferredSizeWidget appbarWidget(
+  BuildContext context, {
+  required String title,
+  Widget? suffixIcon,
+  Color? customColor,
+  Color? customForeground,
+}) {
+  return PreferredSize(
+    preferredSize: Size.fromHeight(60),
+    child: AppBar(
+      elevation: 1,
+      backgroundColor: customColor ?? Colors.white,
+      titleSpacing: 0,
+      automaticallyImplyLeading: false,
+      title: Container(
+        width: double.infinity,
+        padding: EdgeInsets.symmetric(horizontal: margin16),
+        child: Row(
+          children: [
+            GestureDetector(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              child: Icon(
+                Icons.arrow_back,
+                color: customForeground ?? Theme.of(context).primaryColor,
+              ),
             ),
-          ),
-          SizedBox(
-            width: margin24 / 2,
-          ),
-          Expanded(
+            SizedBox(width: margin24 / 2),
+            Expanded(
               child: Text(
-            title,
-            style: mainBody4.copyWith(
-                color: customForeground ?? Colors.black87,
-                fontWeight: FontWeight.bold),
-          )),
-          suffixWidget ?? const SizedBox()
-        ],
+                title,
+                style: mainBody4.copyWith(
+                  color: customForeground ?? Colors.black87,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            suffixIcon ?? const SizedBox(),
+          ],
+        ),
       ),
     ),
   );
