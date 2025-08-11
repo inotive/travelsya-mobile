9e2adc32f687243a1f2f993f17d64b6c49b57415
herliananur <herliananur19@gmail.com>
2025-08-11 15:58:33 +0800

    update splashscreen

--- lib/app/splash_screen/pages/splash_screen_page.dart
+++ lib/app/splash_screen/pages/splash_screen_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 import 'package:travelsya/app/splash_screen/vm/splash_vm.dart';
-// import 'package:travelsya/shared/helper/const_helper.dart';
 import 'package:travelsya/shared/widgets/statusbar_widget.dart';
 
 class SplashScreenPage extends StatelessWidget {
@@ -17,22 +16,14 @@ class SplashScreenPage extends StatelessWidget {
       return StatusbarWidget(
         customBrightness: Brightness.light,
         child: Scaffold(
-          backgroundColor: Colors.white,
-          body: Center(
-            child: Image.asset(
-              'assets/images/logo_splash_screen.png',
-              width: 150,
-              height: 150,
-            ),
+          body: Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage('assets/images/splash_screen_new.jpg'))),
           ),
-          // body: Container(
-          //   width: double.infinity,
-          //   height: double.infinity,
-          //   decoration: const BoxDecoration(
-          //       image: DecorationImage(
-          //           fit: BoxFit.cover,
-          //           image: AssetImage(ConstHelper.splashScreenImage))),
-          // ),
         ),
       );
     });
