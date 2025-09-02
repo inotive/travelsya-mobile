cdb3b1994a456712e30891afe3ff579a1639ba19
herliananur <herliananur19@gmail.com>
2025-09-02 10:19:36 +0800

    Update recreation_main_vm.dart

--- lib/app/rekreasi/viewmodel/recreation_main_vm.dart
+++ lib/app/rekreasi/viewmodel/recreation_main_vm.dart
@@ -1,21 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 import 'package:travelsya/app/rekreasi/cubits/recreation_cubit.dart';
+import 'package:travelsya/app/rekreasi/cubits/recreation_state.dart';
+import 'package:travelsya/app/rekreasi/models/recreation_model.dart';
 import 'package:travelsya/shared/widgets/city_picker_bottomsheet.dart';
 
 class RecreationMainVM extends BaseViewModel {
   RecreationCubit recreationCategoryCubit = RecreationCubit();
   RecreationCubit recreationPreviewCubit = RecreationCubit();
+  RecreationCubit recreationCityCubit = RecreationCubit();
 
   int? selectedCategory;
-
-  String? selectedCity;
+  String? selectedCityName;
+  RecreationCityModel? selectedCity;
 
   onChangeSelectedCity(BuildContext context) async {
-    String? result = await showCityPicker(context);
-
+    final RecreationCityModel? result = await showCityPicker<
+        RecreationCityModel, RecreationCubit, RecreationState>(
+      context,
+      cubit: recreationCityCubit,
+      fetchFunction: (cubit, BuildContext ctx) async {
+        cubit.fetchRecreationAvailableCity(ctx);
+      },
+      isLoading: (state) => state is RecreationLoading,
+      getCities: (state) => state is ListRecreationCityLoaded ? state.data : [],
+      displayName: (item) => item.cityName,
+    );
     if (result != null) {
       selectedCity = result;
+      selectedCityName = result.cityName;
       notifyListeners();
     }
   }
