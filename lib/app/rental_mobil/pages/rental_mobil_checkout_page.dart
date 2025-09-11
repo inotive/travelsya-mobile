760ea47c2d5b8649af00d3710f8092e9cea3ea54
herliananur <herliananur19@gmail.com>
2025-09-11 11:10:12 +0800

    Update rental_mobil_checkout_page.dart

--- lib/app/rental_mobil/pages/rental_mobil_checkout_page.dart
+++ lib/app/rental_mobil/pages/rental_mobil_checkout_page.dart
@@ -509,9 +509,138 @@ class RentalCheckoutPage extends StatelessWidget {
                                       ),
                                     )
                                   ],
-                                )
+                                ),
+                                SizedBox(
+                                  height: margin16,
+                                ),
+                                Stack(
+                                  children: [
+                                    Positioned(
+                                      top: 0,
+                                      bottom: 0,
+                                      left: 0,
+                                      right: 0,
+                                      child: Container(
+                                        width: double.infinity,
+                                        height: double.infinity,
+                                        decoration: BoxDecoration(
+                                            border: Border.all(
+                                                color:
+                                                    neutral50.withOpacity(0.3)),
+                                            borderRadius:
+                                                BorderRadius.circular(10),
+                                            color: Colors.white),
+                                        alignment: Alignment.topRight,
+                                        child: SizedBox(
+                                          width: 45,
+                                          height: 45,
+                                          child: Image.asset(
+                                            'assets/icons/group_23.png',
+                                            fit: BoxFit.cover,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    Container(
+                                      padding: EdgeInsets.all(margin16),
+                                      width: double.infinity,
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            'Deskripsi',
+                                            style: mainBody4.copyWith(
+                                                color: neutral100,
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                          Container(
+                                            margin: EdgeInsets.symmetric(
+                                                vertical: margin24 / 2),
+                                            width: double.infinity,
+                                            height: 1,
+                                            color: neutral50.withOpacity(0.3),
+                                          ),
+                                          Text(
+                                            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                                            style: mainBody4.copyWith(
+                                              color: neutral100,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                SizedBox(
+                                  height: margin16,
+                                ),
+                                Stack(
+                                  children: [
+                                    Positioned(
+                                      top: 0,
+                                      bottom: 0,
+                                      left: 0,
+                                      right: 0,
+                                      child: Container(
+                                        width: double.infinity,
+                                        height: double.infinity,
+                                        decoration: BoxDecoration(
+                                            border: Border.all(
+                                                color:
+                                                    neutral50.withOpacity(0.3)),
+                                            borderRadius:
+                                                BorderRadius.circular(10),
+                                            color: Colors.white),
+                                        alignment: Alignment.topRight,
+                                        child: SizedBox(
+                                          width: 45,
+                                          height: 45,
+                                          child: Image.asset(
+                                            'assets/icons/group_23.png',
+                                            fit: BoxFit.cover,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    Container(
+                                      padding: EdgeInsets.all(margin16),
+                                      width: double.infinity,
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            'Peraturan',
+                                            style: mainBody4.copyWith(
+                                                color: neutral100,
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                          Container(
+                                            margin: EdgeInsets.symmetric(
+                                                vertical: margin24 / 2),
+                                            width: double.infinity,
+                                            height: 1,
+                                            color: neutral50.withOpacity(0.3),
+                                          ),
+                                          Text(
+                                            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                                            style: mainBody4.copyWith(
+                                              color: neutral100,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    )
+                                  ],
+                                ),
                               ],
                             ),
+                          ),
+                          Container(
+                            width: double.infinity,
+                            height: 8,
+                            color: const Color(0xfff4f4f4),
                           ),
                           Container(
                             padding: EdgeInsets.all(margin16),
