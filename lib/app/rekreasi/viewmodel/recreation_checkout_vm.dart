eeac9e95815d707e9076fc547d3f541fd86a16e2
herliananur <herliananur19@gmail.com>
2025-09-12 17:07:17 +0800

    Update recreation_checkout_vm.dart

--- lib/app/rekreasi/viewmodel/recreation_checkout_vm.dart
+++ lib/app/rekreasi/viewmodel/recreation_checkout_vm.dart
@@ -3,16 +3,21 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:stacked/stacked.dart';
 import 'package:travelsya/app/auth/cubits/auth_cubit.dart';
 import 'package:travelsya/app/auth/cubits/auth_state.dart';
+import 'package:travelsya/app/home_main/pages/home_main_page.dart';
+import 'package:travelsya/app/payment/pages/payment_webview_page.dart';
 // import 'package:travelsya/app/home_main/pages/home_main_page.dart';
 // import 'package:travelsya/app/payment/pages/payment_webview_page.dart';
-import 'package:travelsya/app/rekreasi/pages%20new/new_rekreasi_checkout_page.dart';
+// import 'package:travelsya/app/rekreasi/pages%20new/new_rekreasi_checkout_page.dart';
 import 'package:travelsya/app/rekreasi/services/recreation_service.dart';
+import 'package:travelsya/shared/api/api_return_value.dart';
 // import 'package:travelsya/shared/api/api_return_value.dart';
 import 'package:travelsya/shared/cubits/fee_admin/fee_admin_model.dart';
+import 'package:travelsya/shared/cubits/main_index_cubit.dart';
 // import 'package:travelsya/shared/cubits/main_index_cubit.dart';
 import 'package:travelsya/shared/cubits/point/point_cubit.dart';
 import 'package:travelsya/shared/cubits/point/point_state.dart';
 import 'package:travelsya/shared/function/show_loading.dart';
+import 'package:travelsya/shared/function/show_snackbar.dart';
 // import 'package:travelsya/shared/function/show_snackbar.dart';
 
 class RecreationCheckoutVM extends BaseViewModel {
@@ -60,67 +65,45 @@ class RecreationCheckoutVM extends BaseViewModel {
       notifyListeners();
     }
   }
-
-  // onSubmit(
-  //   BuildContext context, {
-  //   required int packageId,
-  // }) {
-  //   showLoading(context);
-  //   RecreationService.checkoutRecreation(context, data: {
-  //     "service": "recreation",
-  //     "payment": "xendit",
-  //     "package_id": packageId.toString(),
-  //     "point": usePoint ? '1' : '0',
-  //     "total_ticket": '1',
-  //     "status": "pending"
-  //   }).then((value) async {
-  //     if (context.mounted) {
-  //       Navigator.pop(context);
-  //       if (value.status == RequestStatus.successRequest) {
-  //         BlocProvider.of<MainIndexCubit>(context).changeIndex(1);
-
-  //         Navigator.pushAndRemoveUntil(
-  //             context,
-  //             MaterialPageRoute(builder: (_) => const HomeMainPage()),
-  //             (route) => false);
-
-  //         Navigator.push(
-  //             context,
-  //             MaterialPageRoute(
-  //                 builder: (_) => UserPaymentWebview(
-  //                       url: value.data,
-  //                     )));
-  //       } else {
-  //         showSnackbar(context,
-  //             data: value.data ?? 'Gagal membuat link pembayaran',
-  //             colors: Colors.orange);
-  //       }
-  //     }
-  //   });
-  // }
-
+  //ONSUBMIT YG SEBELUMNYA
   onSubmit(
     BuildContext context, {
-    required List<CheckoutItem> items,
-    bool usePoint = false,
+    required int packageId,
   }) {
-    final totalTickets = items.fold(0, (sum, e) => sum + e.quantity);
-
     showLoading(context);
     RecreationService.checkoutRecreation(context, data: {
       "service": "recreation",
       "payment": "xendit",
-      "packages": items.map((e) => e.package.id.toString()).toList(),
-      "quantities": items.map((e) => e.quantity.toString()).toList(),
+      "package_id": packageId.toString(),
       "point": usePoint ? '1' : '0',
-      "total_ticket": totalTickets.toString(),
-      "status": "pending"
+      "total_ticket": '1'
     }).then((value) async {
-      Navigator.pop(context);
-      // lanjutkan seperti sebelumnya
+      if (context.mounted) {
+        Navigator.pop(context);
+        if (value.status == RequestStatus.successRequest) {
+          BlocProvider.of<MainIndexCubit>(context).changeIndex(1);
+
+          Navigator.pushAndRemoveUntil(
+              context,
+              MaterialPageRoute(builder: (_) => const HomeMainPage()),
+              (route) => false);
+
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (_) => UserPaymentWebview(
+                        url: value.data,
+                      )));
+        } else {
+          showSnackbar(context,
+              data: value.data ?? 'Gagal membuat link pembayaran',
+              colors: Colors.orange);
+        }
+      }
     });
   }
 
+//PAYMENT PAKET LEBIH DARI 1
   // onSubmit(
   //   BuildContext context, {
   //   required List<CheckoutItem> items,
