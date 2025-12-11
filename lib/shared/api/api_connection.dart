3efaed6b08a2d0e8368c74f67801b0da87310307
herliananur <herliananur19@gmail.com>
2025-12-11 09:42:38 +0800

    Update api_connection.dart

--- lib/shared/api/api_connection.dart
+++ lib/shared/api/api_connection.dart
@@ -1,7 +1,7 @@
 // String baseUrl = 'https://travelsya.amaradvokat.com/';
 // String baseUrl = 'https://travelsya.com/';
-String baseUrl = 'https://dev.travelsya.com/';
-// String baseUrl = 'https://travelsya.inotivedev.com/';
+// String baseUrl = 'https://dev.travelsya.com/';
+String baseUrl = 'https://travelsya.inotivedev.com/';
 String basePathUserUrl = '${baseUrl}storage/public/users/';
 String baseAPIUrl = '${baseUrl}api';
 String baseAPIImageUrl = '${baseUrl}public';
