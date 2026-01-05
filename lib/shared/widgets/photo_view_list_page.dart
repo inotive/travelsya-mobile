6fbe56fff09e9506052cc63cf8ea4a01564d1a58
herliananur <herliananur19@gmail.com>
2026-01-05 09:50:32 +0800

    Update photo_view_list_page.dart

--- lib/shared/widgets/photo_view_list_page.dart
+++ lib/shared/widgets/photo_view_list_page.dart
@@ -4,36 +4,77 @@ import 'package:photo_view/photo_view_gallery.dart';
 
 class PhotoViewListPage extends StatefulWidget {
   final List<String> images;
-  const PhotoViewListPage({super.key, required this.images});
+  final int initialIndex;
+
+  const PhotoViewListPage({
+    super.key,
+    required this.images,
+    this.initialIndex = 0,
+  });
 
   @override
   State<PhotoViewListPage> createState() => _PhotoViewListPageState();
 }
 
 class _PhotoViewListPageState extends State<PhotoViewListPage> {
+  late final PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: widget.initialIndex);
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-        child: PhotoViewGallery.builder(
-      scrollPhysics: const BouncingScrollPhysics(),
-      builder: (BuildContext context, int index) {
-        return PhotoViewGalleryPageOptions(
-          imageProvider: NetworkImage(widget.images[index]),
-          initialScale: PhotoViewComputedScale.contained * 0.9,
-        );
-      },
-      itemCount: widget.images.length,
-      loadingBuilder: (context, event) => Center(
-        child: SizedBox(
-          width: 20.0,
-          height: 20.0,
-          child: CircularProgressIndicator(
-            value: event == null
-                ? 0
-                : event.cumulativeBytesLoaded / event.expectedTotalBytes!,
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Stack(
+        children: [
+          PhotoViewGallery.builder(
+            pageController: _pageController,
+            itemCount: widget.images.length,
+            scrollPhysics: const BouncingScrollPhysics(),
+            builder: (BuildContext context, int index) {
+              return PhotoViewGalleryPageOptions(
+                imageProvider: NetworkImage(widget.images[index]),
+                minScale: PhotoViewComputedScale.contained,
+                initialScale: PhotoViewComputedScale.contained,
+                maxScale: PhotoViewComputedScale.covered * 3,
+                heroAttributes:
+                    PhotoViewHeroAttributes(tag: widget.images[index]),
+              );
+            },
+            loadingBuilder: (context, event) => Center(
+              child: SizedBox(
+                width: 24,
+                height: 24,
+                child: CircularProgressIndicator(
+                  value: event == null || event.expectedTotalBytes == null
+                      ? null
+                      : event.cumulativeBytesLoaded /
+                          event.expectedTotalBytes!,
+                ),
+              ),
+            ),
           ),
-        ),
+
+          Positioned(
+            top: MediaQuery.of(context).padding.top + 8,
+            right: 12,
+            child: IconButton(
+              icon: const Icon(Icons.close, color: Colors.white, size: 28),
+              onPressed: () => Navigator.pop(context),
+            ),
+          ),
+        ],
       ),
-    ));
+    );
   }
 }
